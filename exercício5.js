48da4a3de71647eeba388c47d3bbf0669bed8225
mjvmac <j.venancio@aluno.ifsp.edu.br>
2025-04-11 14:57:25 -0300

    Update exercício5.js

--- exercício5.js
+++ exercício5.js
@@ -2,4 +2,12 @@ let idade = 45
 if idade <=12{
     console.log("Tipo de ingresso: infantil")
 }
-else if idade > 12
+else if idade > 12 && idade <=17{
+    console.log("Tipo de ingresso: adolescente")
+}
+else if idade >= 18 && idade >=59{
+    console.log("Tipo de ingresso: adulto")
+}
+else{
+    console.log("Tipo de ingresso: sênior")
+}
